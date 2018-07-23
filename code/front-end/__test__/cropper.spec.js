ed31c31dd84ac3d59c34a8a384747f3fa73909d7
changeyoung98 <chenzhiyang99@126.com>
2018-07-23 22:09:04 +0800

    update unit test

--- code/front-end/__test__/cropper.spec.js
+++ code/front-end/__test__/cropper.spec.js
@@ -2,10 +2,11 @@
  * Created by hp on 2018/7/19.
  */
 import React from 'react'
-import Cropper from '../js/Cropper'
+import Crop from '../js/Cropper'
+import Cropper from 'react-cropper';
 import {Button,Input,Icon} from 'antd'
 import toJson from 'enzyme-to-json';
-import { shallow, configure,render } from 'enzyme'
+import { shallow, configure,render,mount } from 'enzyme'
 import  Adapter from 'enzyme-adapter-react-14';
 
 configure({ adapter: new Adapter() });
@@ -23,20 +24,15 @@ const setup = () => {
     useDefaultImage :jest.fn(),
     load :jest.fn()
     };
-    const wrapper = shallow(<Cropper />)
+    const wrapper = shallow(<Crop />)
+
     return {
         props,
-        wrapper
+        wrapper,
     }
-}
+};
 describe('Cropper render', () => {
    const {wrapper,props}=setup();
-    it('basic use', () => {
-        const wrapper = render(
-            <Cropper style={{ height: 400, width: '100%' }} />
-        );
-        expect(toJson(wrapper)).toMatchSnapshot();
-    });
         it('Button Component should render', () => {
             //.find(selector) 是 Enzyme shallow Rendering 提供的语法, 用于查找节点
             // 详细用法见 Enzyme 文档 http://airbnb.io/enzyme/docs/api/shallow.html
@@ -52,4 +48,41 @@ describe('Cropper render', () => {
         // 详细用法见 Enzyme 文档 http://airbnb.io/enzyme/docs/api/shallow.html
         expect(wrapper.find('img').length).toBe(1);
     });
+    it('basic use', () => {
+        const wrapperS = render(
+            <Crop {...props} />
+        );
+        expect(toJson(wrapperS)).toMatchSnapshot();
+    });
+
+/*describe('function',()=>{
+    const {props,wrapperM}=setup();
+ it('Button Component should click', () => {
+ const buttonDom=wrapperM.find('Button').at(0);
+ buttonDom.simulate('click');
+ expect(props.load).toBeCalled;
+ });
+
+*/
+});
+ describe('function',()=> {
+    const src = 'child.jpg';
+    const {props} = setup();
+    const wrapperM = shallow(<Crop {...props}/>);
+
+    it('Button Component should click', () => {
+        const buttonDom = wrapperM.find('Button').at(1);
+        buttonDom.simulate('click');
+        expect(props.useDefaultImage).toBeCalled;
+    });
+   /* it('Button2 Component should click', () => {
+
+        const mockEvent={
+            src: src,
+            ref: 'cropper'
+        }
+        const buttonDom = wrapperM.find('Button').at(2);
+        buttonDom.simulate('click',mockEvent);
+        expect(props.cropImage).toBeCalled;
+    });*/
 });
